566c72f8c75e9dc59e099f420ba6b215a15b21b8
Stefan Lankes <slankes@eonerc.rwth-aachen.de>
2023-04-26 18:20:40 +0200

    use tock_registers to enable / disable interrupts
    - consequently, we removed inline assembly

--- src/irq.rs
+++ src/irq.rs
@@ -1,31 +1,16 @@
-use core::arch::asm;
+use aarch64_cpu::registers::DAIF;
+use tock_registers::interfaces::{ReadWriteable, Readable, Writeable};
 
-pub const IRQ_FLAG_F: u64 = 1 << 6;
-pub const IRQ_FLAG_I: u64 = 1 << 7;
-pub const IRQ_FLAG_A: u64 = 1 << 8;
-
-/// Enable all interrupts
+/// Enable interrupts
 #[inline]
 pub fn enable() {
-    unsafe {
-        asm!(
-            "msr daifclr, {mask}",
-            mask = const 0b111,
-            options(nostack, nomem),
-        );
-    }
+    DAIF.modify(DAIF::A::Masked + DAIF::I::Masked + DAIF::F::Masked);
 }
 
-/// Disable all interrupts
+/// Disable interrupts
 #[inline]
 pub fn disable() {
-    unsafe {
-        asm!(
-            "msr daifset, {mask}",
-            mask = const 0b111,
-            options(nostack, nomem),
-        );
-    }
+    DAIF.modify(DAIF::A::Unmasked + DAIF::I::Unmasked + DAIF::F::Unmasked);
 }
 
 /// Disable IRQs (nested)
@@ -35,23 +20,10 @@ pub fn disable() {
 /// in situations when interrupts shouldn't be activated if they
 /// were not activated before calling this function.
 #[inline]
-pub fn nested_disable() -> bool {
-    let flags: u64;
-    unsafe {
-        asm!(
-            "mrs {}, daif",
-            out(reg) flags,
-            options(nostack, nomem),
-        );
-    }
-
-    let mut was_enabled = true;
-    if flags & (IRQ_FLAG_A | IRQ_FLAG_I | IRQ_FLAG_F) > 0 {
-        was_enabled = false;
-    }
-
+pub fn nested_disable() -> u64 {
+    let bits = DAIF.get();
     disable();
-    was_enabled
+    bits
 }
 
 /// Enable all IRQs (nested)
@@ -59,8 +31,6 @@ pub fn nested_disable() -> bool {
 /// Can be used in conjunction with nested_disable() to only enable
 /// interrupts again if they were enabled before.
 #[inline]
-pub fn nested_enable(was_enabled: bool) {
-    if was_enabled {
-        enable();
-    }
+pub fn nested_enable(bits: u64) {
+    DAIF.set(bits);
 }
